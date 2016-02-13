4d054d2e9bc3637ec97572eaf8c5b82081f9b270
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2016-02-13 23:46:01 +0200

    Fix name conflict

--- stormssh-completion.rb
+++ stormssh-completion.rb
@@ -1,17 +1,17 @@
 class StormSshCompletion < Formula
   desc "Completion for storm-ssh"
   homepage "https://github.com/vigo/stormssh-completion"
-  url "https://github.com/vigo/stormssh-completion/archive/0.1.0.tar.gz"
-  sha256 "fa1ee4113e4e64109bb35f53a54b08c5f1ce9995b48c204aae7a50bd983a5dfe"
+  url "https://github.com/vigo/stormssh-completion/archive/0.1.1.tar.gz"
+  sha256 "cbdc35d674919aacc18723c42f2b6354fcd3efdcbfbb28e1fe60fbd1c1c7b18d"
   
   bottle :unneeded
 
   def install
-    bash_completion.install "storm"
+    bash_completion.install "stormssh"
   end
 
   test do
-    assert_match "-F __storm",
-      shell_output("source #{bash_completion}/storm && complete -p storm")
+    assert_match "-F __stormssh",
+      shell_output("source #{bash_completion}/stormssh && complete -p storm")
   end
 end
